c5c86ff9ffdb87e9afe0d03ff13d736c76a60ca5
Sar Champagne Bielert <sar@codepath.org>
2023-08-11 10:37:38 -0600

    Add unit 3 project scripts and cleanup naming

--- Scripts/unit3_lab.sh
+++ Scripts/unit3_lab.sh
@@ -5,21 +5,21 @@ none='\033[0m'
 
 echo "[UNIT 3 LAB] Starting script..."
 
-# Copy the crackfiles into the unit3_lab directory
-if [ -e "$HOME/unit3_lab/crackfiles.zip" ]; then
-    echo -e "[UNIT 3 LAB] File crackfiles.zip already found at ~/unit3_lab."
+# Copy the required file into the unit3 directory
+if [ -e "$HOME/unit3/crackfiles.zip" ]; then
+    echo -e "[UNIT 3 LAB] File crackfiles.zip already found at ~/unit3."
 else
-    if [ -e "$HOME/Files/unit_3/crackfiles.zip" ]; then
-        mkdir -p "$HOME/unit3_lab"
-        echo "[UNIT 3 LAB] Copying crackfiles.zip to unit3_lab/crackfiles.zip..."
-        sudo cp "$HOME/Files/unit_3/crackfiles.zip" "$HOME/unit3_lab/crackfiles.zip"
+    if [ -e "$HOME/Files/unit3/crackfiles.zip" ]; then
+        mkdir -p "$HOME/unit3"
+        echo "[UNIT 3 LAB] Copying crackfiles.zip to unit3/crackfiles.zip..."
+        sudo cp "$HOME/Files/unit3/crackfiles.zip" "$HOME/unit3/crackfiles.zip"
     else
-        echo -e "${red}[UNIT 3 LAB]${none} Error: File ~/Files/unit_3/crackfiles.zip does not exist."
+        echo -e "${red}[UNIT 3 LAB]${none} Error: File ~/Files/unit3/crackfiles.zip does not exist."
         exit 1
     fi
     # Verify copy was successful
-    if ! [ "$HOME/unit3_lab/crackfiles.zip" ]; then
-        echo -e "${red}[UNIT 3 LAB]${none} Error: Could not copy crackfiles.zip to unit_3/crackfiles.zip"
+    if ! [ "$HOME/unit3/crackfiles.zip" ]; then
+        echo -e "${red}[UNIT 3 LAB]${none} Error: Could not copy crackfiles.zip to unit3/crackfiles.zip"
         exit 1
     fi
 fi
