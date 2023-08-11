b0ebc4f5b4f66a7559d0398dc0da0125ddeabe9c
Sar Champagne Bielert <sar@codepath.org>
2023-08-11 10:16:10 -0600

    Logic bug fix

--- Scripts/unit3_lab.sh
+++ Scripts/unit3_lab.sh
@@ -5,24 +5,6 @@ none='\033[0m'
 
 echo "[UNIT 3 LAB] Starting script..."
 
-# Check if the script needs to run
-if command -v john >/dev/null 2>&1 ; then
-    echo -e "${green}[UNIT 3 LAB]${none} John is already installed."
-    exit 0
-fi
-echo "[UNIT 3 LAB] Installing John..."
-
-# Install John
-sudo apt-get install john -y
-
-# Print output based on whether or not John is installed
-if command -v john >/dev/null 2>&1 ; then
-    echo -e "${green}[UNIT 3 LAB]${none} John installed successfully."
-else
-    echo -e "${red}[UNIT 3 LAB]${none} ERROR: John was not installed correctly!"
-    exit 1
-fi
-
 # Move the crackfiles into the unit3_lab directory
 if [ -e "$HOME/unit3_lab/crackfiles.zip" ]; then
     echo -e "[UNIT 3 LAB] File crackfiles.zip already found at ~/unit3_lab."
@@ -39,4 +21,22 @@ else
         echo -e "${red}[UNIT 3 LAB]${none} Error: Could not move crackfiles.zip to ~/Files/unit_3/crackfiles.zip"
         exit 1
     fi
+fi
+
+# Check if the script needs to run
+if command -v john >/dev/null 2>&1 ; then
+    echo -e "${green}[UNIT 3 LAB]${none} John is already installed."
+    exit 0
+fi
+echo "[UNIT 3 LAB] Installing John..."
+
+# Install John
+sudo apt-get install john -y
+
+# Print output based on whether or not John is installed
+if command -v john >/dev/null 2>&1 ; then
+    echo -e "${green}[UNIT 3 LAB]${none} John installed successfully."
+else
+    echo -e "${red}[UNIT 3 LAB]${none} ERROR: John was not installed correctly!"
+    exit 1
 fi
