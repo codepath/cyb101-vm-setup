9a9d2d588c491e56ebc17e5abbc76906e8ca9c27
Sar Champagne Bielert <sar@codepath.org>
2023-08-14 16:45:01 -0600

    Add unit 4 lab script

--- Scripts/unit4_lab.sh
+++ Scripts/unit4_lab.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 none='\033[0m'
-yellow='\033[1;33m'
+red='\033[0;31m'
+green='\033[0;32m'
 echo "[UNIT 4 LAB] Starting script..."
-echo -e "${yellow}[UNIT 4 LAB]${none} WARNING: SCRIPT NOT YET IMPLEMENTED"
+
+# First, check if firefox is installed
+if command -v firefox &> /dev/null; then
+    echo -e "${green}[UNIT 4 LAB]${none} Firefox is already installed."
+else
+    sudo apt install -y firefox
+    if ! command -v steghide &> /dev/null; then
+        echo -e "${red}[UNIT 4 LAB]${none} ERROR: Firefox did not install!"
+        exit 1
+    fi
+fi
