784b00e58b935c2bc00ee1e8cc94046a6633cc40
Sar Champagne Bielert <sar@codepath.org>
2023-08-15 16:11:46 -0600

    Add dependencies to unit 8 script

--- Scripts/unit8_lab.sh
+++ Scripts/unit8_lab.sh
@@ -6,17 +6,38 @@ echo "[UNIT 8 LAB] Starting script..."
 # First, check if steghide is installed
 if command -v setoolkit &> /dev/null; then
     echo -e "${green}[UNIT 8 LAB]${none} setoolkit is already installed."
-else
-    git clone https://github.com/trustedsec/social-engineer-toolkit/ setoolkit/
-    cd setoolkit
-    pip3 install -r requirements.txt
-    python setup.py
-    if ! command -v setoolkit &> /dev/null; then
-        echo -e "${red}[UNIT 8 LAB]${none} ERROR: setoolkit did not install!"
+    exit 0
+fi
+
+# Next, check if python is installed
+if ! command -v python &> /dev/null; then
+    sudo apt install -y python-is-python3
+    if ! command -v python &> /dev/null; then
+        echo -e "${red}[UNIT 8 LAB]${none} ERROR: python-is-python3 did not install!"
         exit 1
-    else
-        echo -e "${green}[UNIT 8 LAB]${none} setoolkit installed!"
     fi
 fi
+
+# Next, check if pip3 is installed
+if ! command -v pip3 &> /dev/null; then
+    sudo apt install -y python3-pip
+    if ! command -v pip3 &> /dev/null; then
+        echo -e "${red}[UNIT 8 LAB]${none} ERROR: python3-pip did not install!"
+        exit 1
+    fi
+fi
+
+git clone https://github.com/trustedsec/social-engineer-toolkit/ setoolkit/
+cd setoolkit
+pip3 install -r requirements.txt
+python setup.py
+
+if ! command -v setoolkit &> /dev/null; then
+    echo -e "${red}[UNIT 8 LAB]${none} ERROR: setoolkit did not install!"
+    exit 1
+else
+    echo -e "${green}[UNIT 8 LAB]${none} setoolkit installed!"
+fi
+
 
 
